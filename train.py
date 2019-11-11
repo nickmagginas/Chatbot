96a3d6909a9e8405a2b783e49db5b5f29b2f34c8
nickmagginas <nickmanginas@gmail.com>
2019-11-11 21:54:48 +0000

    Improved training

--- train.py
+++ train.py
@@ -2,9 +2,10 @@ import encoder as e
 import decoder as d
 import read_dialogues as reader
 import torch
+from time import sleep
 
 FILENAME = 'data/dialogues/AGREEMENT_BOT.txt'
-HIDDEN_SIZE = 512
+HIDDEN_SIZE = 256
 
 
 ### Main Training Routine
@@ -15,34 +16,42 @@ def main(verbose = False):
     length = dictionary.__len__()
     encoder = e.Encoder(length, HIDDEN_SIZE)
     decoder = d.Decoder(HIDDEN_SIZE, length)
-    encoder_optimizer = torch.optim.SGD(encoder.parameters(), 0.001)
-    decoder_optimizer = torch.optim.SGD(decoder.parameters(), 0.001)
+    encoder_optimizer = torch.optim.SGD(encoder.parameters(), 0.0005)
+    decoder_optimizer = torch.optim.SGD(decoder.parameters(), 0.0005)
     loss_function = torch.nn.NLLLoss()
     SOS = torch.LongTensor([1]).view(-1, 1)
     for index, pair in enumerate(pairs):
+        if verbose:
+            print(reader.translate(pair[0], rev))
+            print(reader.translate(pair[1], rev))
         seq_length = pair[1].index(2)
         state = encoder.init_state(HIDDEN_SIZE)
         propagate = e.feed_sentence(encoder, pair[0], state)
         acc = d.feed_forward(decoder, SOS, propagate , seq_length)
         predictions = torch.cat(acc)
+        translatable = torch.argmax(predictions, dim = 1).numpy()
+        translated = reader.translate(translatable, rev)
         target = torch.LongTensor(pair[1][1::])
         loss = loss_function(predictions, target)
         decoder_optimizer.zero_grad()
         encoder_optimizer.zero_grad()
         loss.backward()
         if verbose:
-            print(f'Q: {reader.translate(pair[0], rev)}')
+            print(f'Q: {reader.translate(pair[0][1::], rev)}')
             print(f'A: {reader.translate(pair[1], rev)}')
             print(f'Encoder State Dimension: {propagate.shape}')
-            print(f'Predictions Shape: {predictions.shape}')
-            print(f'Target Shape: {target.shape}')
+            print(f'Predictions: {translated}')
+            print(f'Target:  {reader.translate(target.numpy(), rev)}')
 
         print(f'Iteration: {index}, Loss: {loss.item()}')
         decoder_optimizer.step()
         encoder_optimizer.step()
+        if verbose: sleep(1)
+    torch.save(encoder.state_dict(), 'encoder')
+    torch.save(decoder.state_dict(), 'decoder')
     return encoder, decoder
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': main(verbose = False)
 
 
