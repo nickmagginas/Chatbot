cae8f631826f377fbec10bd24866d562c33aac20
nickmanginas <nickmanginas@gmail.com>
2019-11-15 17:38:10 +0100

    Modified Decoder

--- decoder.py
+++ decoder.py
@@ -6,10 +6,10 @@ class Decoder(torch.nn.Module):
         self.embedding = torch.nn.Embedding(output_size, hidden_size)
         self.gru = torch.nn.GRU(hidden_size, hidden_size)
         self.fc = torch.nn.Linear(hidden_size, output_size)
-        self.softmax = torch.nn.Softmax(dim = 2)
+        self.softmax = torch.nn.LogSoftmax(dim = 2)
 
     def forward(self, x, s):
-        embedded = self.embedding(x)
+        embedded = torch.nn.functional.relu(self.embedding(x))
         reccurent_weights, hidden = self.gru(embedded, s)
         output = self.fc(reccurent_weights)
         return self.softmax(output), hidden
