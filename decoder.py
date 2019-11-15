2cbfb422005f0a252b05b4e70633a65eba9e040a
nickmagginas <nickmanginas@gmail.com>
2019-11-15 17:00:27 +0000

    Removed Attention

--- decoder.py
+++ decoder.py
@@ -16,28 +16,3 @@ class Decoder(torch.nn.Module):
 
 ##################################################################################
 
-'''
-class Decoder(torch.nn.Module):
-    def __init__(self, output_size, hidden_size):
-        super(Decoder, self).__init__()
-        self.embedding = torch.nn.Embedding(output_size, hidden_size)
-        self.attention = torch.nn.Linear(3 * hidden_size, 1)
-        self.softmax_attention = torch.nn.Softmax()
-        self.gru = torch.nn.GRU(hidden_size, hidden_size)
-        self.fc = torch.nn.Linear(hidden_size, output_size)
-
-    def forward(self, x, s, encoder_outputs):
-        sequence_length = encoder_outputs.shape[1]
-        repeated_state = s.repeat(1, sequence_length, 1)
-        embedded = self.embedding(x)
-        repeated_input = embedded.repeat(1, sequence_length, 1)
-        print(repeated_state.shape)
-        print(repeated_input.shape)
-
-encoder_outputs = torch.randn(53, 3, 32)
-encoder_state = torch.randn(1, 3, 32)
-sample = torch.randint(0, 3, (53, 1))
-
-decoder = Decoder(1501, 32)
-decoder(sample, encoder_state, encoder_outputs)
-'''
