63ba86354199f4f2fd13155a453154060ad02f7a
nickmanginas <nickmanginas@gmail.com>
2019-11-15 17:38:25 +0100

    Evaluator

--- evaluate.py
+++ evaluate.py
@@ -2,13 +2,14 @@ import read_files as r
 import torch
 from encoder import Encoder
 from decoder import Decoder
+from time import sleep
 
 def evaluate_loop(encoder, decoder, dictionary):
     reverse_dictionary = r.reverse_dictionary(dictionary)
     while True:
         question = r.normalize_sentence(input('Q:'))
         question_length = len(question.split())
-        encoder_state = encoder.__init_hidden__(64).repeat(1, 1, 1)
+        encoder_state = encoder.__init_hidden__(512).repeat(1, 1, 1)
         encoded_sentence = r.encode_sentence(question, dictionary)
         query = torch.tensor(encoded_sentence, dtype = torch.long).view(-1, 1)
         _ , decoder_state = encoder(query, encoder_state)
@@ -19,11 +20,16 @@ def evaluate_loop(encoder, decoder, dictionary):
             decoder_input = torch.argmax(decoder_output, dim = 2)
             word_index = decoder_input.item()
             final_output = final_output + [word_index]
+            print(r.decode_sentence(final_output, reverse_dictionary))
+            sleep(1)
             if word_index == 1: break
         print(r.decode_sentence(final_output, reverse_dictionary))
 
-pairs, dictionary = r.create_dataset('data/dialogues/AGREEMENT_BOT.txt')
-encoder = Encoder(dictionary.__len__(), 64)
-decoder = Decoder(dictionary.__len__(), 64)
+pairs, dictionary = r.create_whole_corpus()
+print(len(dictionary))
+encoder = Encoder(dictionary.__len__(), 512)
+decoder = Decoder(dictionary.__len__(), 512)
+encoder.load_state_dict(torch.load('encoder'))
+decoder.load_state_dict(torch.load('decoder'))
 evaluate_loop(encoder, decoder, dictionary)
 
